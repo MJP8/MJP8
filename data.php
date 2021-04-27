334b7ecf915fedeaf0ab9384f278198ec49891b6
MJP8 <malachi@timelapseplus.com>
2021-04-27 11:48:16 -0500

    added 'fav_langs' property to data.json

--- data.php
+++ data.php
@@ -8,12 +8,6 @@
 </head>
 <body>
 	<?php
-	$script = 
-	"<script src=\"js/jquery.min.js\"></script>
-	<script>
-		$(\"ul\").css(\"padding-left\", ((($(window).width() - $(\"ul\").width()) / 2) + \"px\"));
-	</script>
-	";
 	$json = fopen("data.json", "r") or die('File load error');
 	$json_size = filesize("data.json");
 	$json_data = fread($json, $json_size);
@@ -21,13 +15,17 @@
 	foreach ($php_data as $php_key => $php_key_value) {
 		if (gettype($php_key_value) === 'array') {
 			foreach ($php_key_value as $key => $value) {
-				if ($php_key_value[0] === "Malachi" && $value === "Malachi") {
+				if ($php_key === "name" && $key === 0) {
 					echo "<h1>$value ";
-				} elseif ($php_key_value[0] === "Malachi" && $value === "Parker") {
+				} elseif ($php_key === "name" && $key === count($php_key_value) - 1) {
 					echo "$value</h1>";
-				} elseif ($value === $php_key_value[0] && $php_key_value[0] === "HTML") {
-					echo "<h3>Programming languages</h3><ul><li>$value</li>";
-				} elseif ($value === $php_key_value[12] && $php_key_value[0] === "HTML") {
+				} elseif ($php_key === "programming_langs" && $key === 0) {
+					echo "<h3>Programming languages</h3><ul class=\"langs\"><li>$value</li>";
+				} elseif ($php_key === "programming_langs" && $key === count($php_key_value) - 1) {
+					echo "<li>$value</li></ul>";
+				} elseif ($php_key === "fav_langs" && $key === 0) {
+					echo "<h3>Favorite programming languages</h3><ul class=\"favs\"><li>$value</li>";
+				} elseif ($php_key === "fav_langs" && $key === count($php_key_value) - 1) {
 					echo "<li>$value</li></ul>";
 				} else {
 					echo "<li>$value</li>";	
@@ -37,7 +35,10 @@
 			echo "<h2>$php_key_value</h2>";
 		}
 	}
-	echo $script;
 	?>
+	<script src="js/jquery.min.js"></script>
+	<script>
+		$("ul").css("padding-left", ((($(window).width() - $("ul").width()) / 2) + "px"));
+	</script>
 </body>
 </html>
